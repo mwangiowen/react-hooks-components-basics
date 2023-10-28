ff1892fdc1c7dd024369140764ab73ac532cc20c
mwangiowennjugunasmith <owen.njugun@student.moringa.com>
2023-10-28 23:23:03 +0300

    Done

--- src/Article.js
+++ src/Article.js
@@ -2,8 +2,10 @@ import React from "react";
 
 function Article() {
   return (
+    <div>
     <div>Dear Reader: Bjarne Stroustrup has the perfect lecture oration.</div>
-  );
+    </div>
+    );
 }
 
 export default Article;
